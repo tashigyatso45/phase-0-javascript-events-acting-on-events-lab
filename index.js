5ae390387dcc35101cd84f4cea9609ac0dd30314
Tashi Gyatso <Tashigyatso45@gmail.com>
2023-09-04 17:33:22 -0400

    java event acting complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Your code here
+const dodger= document.getElementById("dodger");
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    let left = parseInt(dodger.style.left);
+    if (left < 360) {
+      left += 10; 
+      dodger.style.left = `${left}px`;
+    }
+  }
